11f7316e173a9ab747bcdb8e791824275039cbc7
William Di Luigi <williamdiluigi@gmail.com>
2015-08-11 01:34:26 +0200

    Add setup.py and cmsocialSyncTasks script

---
+++ cmsocial/scripts/synctasks.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from cms.db import SessionGen, Task
+from cmsocial.db.socialtask import SocialTask
+
+def main():
+    with SessionGen() as s:
+        for task in s.query(Task).all():
+            if not s.query(SocialTask).filter(SocialTask.id == task.id).first():
+                if raw_input("Task " + task.name + " doesn't have metadata. Create? [Y/n]") in ["y", "Y", ""]:
+                    x = SocialTask()
+                    x.id = task.id
+                    s.add(x)
+                    # FIXME: why doesn't this work?
+                    # s.add(SocialTask(id=task.id))
+        s.commit()
---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""Build and installation routines for CMSocial.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+# setuptools doesn't seem to like this:
+# from __future__ import unicode_literals
+
+import os
+
+from setuptools import setup, find_packages
+
+
+PACKAGE_DATA = {
+    "cmsocial": [
+        os.path.join("assets", "*.*"),
+        os.path.join("assets", "css", "*.*"),
+        os.path.join("assets", "js", "*.*"),
+        os.path.join("assets", "views", "*.*"),
+        os.path.join("assets", "views", "*.*"),
+    ],
+}
+
+
+setup(
+    name="cmsocial",
+    version="0.1",
+    author="algorithm-ninja",
+    author_email="algorithm@ninja",
+    url="https://github.com/algorithm-ninja/cmsocial",
+    download_url="https://github.com/algorithm-ninja/cmsocial/archive/master.tar.gz",
+    description="A web application that builds a social coding platform upon CMS",
+    packages=find_packages(),
+    package_data=PACKAGE_DATA,
+    entry_points={
+        "console_scripts": [
+            "cmsocialServer=cmsocial.server.pws:main",
+            "cmsocialInitDB=cmsocial.db:init_db",
+            "cmsocialSyncTasks=cmsocial.scripts.synctasks:main",
+            # "cmsocialSyncUsers=cmsocial.scripts.syncusers:main"
+        ]
+    },
+    keywords="ioi programming contest grader management system",
+    license="Affero General Public License v3",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: "
+        "GNU Affero General Public License v3",
+    ]
+)
