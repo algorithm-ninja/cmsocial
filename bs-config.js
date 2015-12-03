5f48c17952f2585860523206b7e818b8a731b557
William Di Luigi <williamdiluigi@gmail.com>
2015-12-03 23:13:53 +0100

    Improve bs-config.js

--- bs-config.js
+++ bs-config.js
@@ -6,102 +6,23 @@
  | For up-to-date information about the options:
  |   http://www.browsersync.io/docs/options/
  |
- | There are more options than you see here, these are just the ones that are
- | set internally. See the website for more info.
- |
- |
  */
 
-var url = require('url')
-var proxy = require('proxy-middleware')
+const url = require('url')
+const proxy = require('proxy-middleware')
+const fs = require('fs')
+const ini = require('ini')
+
+var config = ini.parse(fs.readFileSync('config/cmsocial.ini', 'utf-8'))
 
 proxyOptions = url.parse('http://localhost:8888')
-proxyOptions.route = "/api"
+proxyOptions.route = config.core.api_prefix
 
 module.exports = {
-    "ui": {
-        "port": 3001,
-        "weinre": {
-            "port": 8080
-        }
-    },
-    "files": false,
-    "watchOptions": {},
     "server": {
       "baseDir": "cmsocial-web.build",
       "middleware": [proxy(proxyOptions)]
     },
-    "proxy": false,
     "port": 3000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
-        }
-    },
-    "logLevel": "info",
-    "logPrefix": "BS",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": false,
-    "open": "local",
-    "browser": "default",
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": true,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 0,
-    "plugins": [],
-    "injectChanges": true,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
-        "scroll",
-        "scroll:element",
-        "input:text",
-        "input:toggles",
-        "form:submit",
-        "form:reset",
-        "click"
-    ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
-        },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
-        },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
-        }
-    },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
-    }
+    "files": []  // TODO: add files to watch
 };
