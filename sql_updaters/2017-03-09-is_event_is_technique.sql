aacf20150a57b26d69a73e3fd7be751c358d76e1
William Di Luigi <williamdiluigi@gmail.com>
2017-04-01 19:36:41 +0200

    Fix sql updater

--- sql_updaters/2017-03-09-is_event_is_technique.sql
+++ sql_updaters/2017-03-09-is_event_is_technique.sql
@@ -1,8 +1,8 @@
 begin;
 
-alter table tags add is_event default 'f' not null;
+alter table tags add is_event boolean default 'f' not null;
 alter table tags alter is_event drop default;
-alter table tags add is_technique default 'f' not null;
+alter table tags add is_technique boolean default 'f' not null;
 alter table tags alter is_technique drop default;
 
 rollback;
