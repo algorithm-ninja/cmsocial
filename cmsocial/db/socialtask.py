b78fdbbff3bbe1b5640640c27edb6f755046973c
William Di Luigi <williamdiluigi@gmail.com>
2017-09-29 19:46:13 +0200

    Fix

--- cmsocial/db/socialtask.py
+++ cmsocial/db/socialtask.py
@@ -18,7 +18,6 @@ from sqlalchemy.orm import backref, relationship
 from sqlalchemy.ext.orderinglist import ordering_list
 
 from cms.db import Task, Participation
-from cms.db.smartmappedcollection import smart_mapped_collection
 from cms import SCORE_MODE_MAX, SCORE_MODE_MAX_TOKENED_LAST
 
 from cmsocial.db.base import Base
