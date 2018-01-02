ccccb330ee4bf7aee02b1affd28f4e7a743c5f16
Luca Versari <veluca93@gmail.com>
2018-01-02 16:06:44 +0100

    Remove score scaling from cmsocialUpdateScore

--- cmsocial/scripts/update_score.py
+++ cmsocial/scripts/update_score.py
@@ -21,12 +21,6 @@ except:
     from cmsocial.db.socialuser import SocialUser as ScoreClass
 
 
-def compute_standard_score(user_to_task, task_to_user):
-    return [(uid, int(sum(map(lambda x: x[1], lst))))
-            for uid, lst in user_to_task.iteritems()
-            ], [(tid, 1.0) for tid in task_to_user.keys()]
-
-
 def get_score(s):
     return 1 if s >= 99 else s / 200.
 
@@ -38,10 +32,6 @@ def compute_smart_score(user_to_task, task_to_user):
     abilities = [1. for x in range(maxuser)]
     attempts = [len(task_to_user.get(i, [])) for i in range(maxtask)]
     attempts_sqrt = map(math.sqrt, attempts)
-
-    pts_per_user = sum(
-        map(lambda x: sum(map(lambda y: y[1], x)),
-            user_to_task.itervalues())) / len(user_to_task)
 
     for uid in range(maxuser):
         if len(user_to_task.get(uid, [])) == 0:
@@ -83,19 +73,12 @@ def compute_smart_score(user_to_task, task_to_user):
             if difficulties[tid] < 0.1:
                 difficulties[tid] = 0.1
 
-    return [(uid, int(pts_per_user * abilities[uid]))
-            for uid in user_to_task.keys()], [(tid, difficulties[tid])
-                                              for tid in task_to_user.keys()]
+    return [(tid, difficulties[tid]) for tid in task_to_user.keys()]
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Update the score of the users/participations")
-    parser.add_argument(
-        "-s",
-        "--standard",
-        action="store_true",
-        help="Use the standard scoring method (sum of a task's scores)")
     args, _ = parser.parse_known_args()
 
     with SessionGen() as session:
@@ -112,20 +95,23 @@ def main():
         for uid, tid, pt in scores:
             user_to_task[uid] = user_to_task.get(uid, []) + [(tid, pt)]
             task_to_user[tid] = task_to_user.get(tid, []) + [(uid, pt)]
-        if args.standard:
-            scores, values = compute_standard_score(user_to_task, task_to_user)
-        else:
-            scores, values = compute_smart_score(user_to_task, task_to_user)
 
-        scores = dict(scores)
-        values = dict(values)
-        sv = session.query(ScoreClass).all()
-        for v in sv:
-            if v.id in scores:
-                v.score = scores[v.id]
+        values = dict(compute_smart_score(user_to_task, task_to_user))
+
+        scores = dict()
+        for uid, problems in user_to_task.iteritems():
+            score = 0
+            for tid, pt in problems:
+                score += values[tid] * pt
+
         st = session.query(SocialTask).all()
         for t in st:
             if t.id in values:
                 t.score_multiplier = values[t.id]
+
+        sv = session.query(ScoreClass).all()
+        for v in sv:
+            if v.id in scores:
+                v.score = scores[v.id]
 
         session.commit()
