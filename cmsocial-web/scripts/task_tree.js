52552bd8c1e313ec2ff70e2d15f37be593c5e0e5
Gemma Martini <martini.gemma3@gmail.com>
2019-05-19 17:57:15 +0200

    added check of no task

--- cmsocial-web/scripts/task_tree.js
+++ cmsocial-web/scripts/task_tree.js
@@ -3,20 +3,20 @@
 /* Tasks page */
 
 angular.module('cmsocial')
-  .controller('TaskTree', function($scope, $stateParams, $state, $http,
-      notificationHub, userManager, l10n, API_PREFIX) {
-    $http.post(API_PREFIX + 'task', {
-      'tag':      'tree',  // can be null
-      'first':    0,
-      'last':     50,
-      'username': userManager.getUser().username,
-      'token':    userManager.getUser().token,
-      'action':   'list'
-    })
-    .success(function(data, status, headers, config) {
-      $scope.tasks = data['tasks'];
-    })
-    .error(function(data, status, headers, config) {
-      notificationHub.serverError(status);
+    .controller('TaskTree', function($scope, $stateParams, $state, $http,
+                                     notificationHub, userManager, l10n,
+                                     API_PREFIX) {
+      $http
+          .post(API_PREFIX + 'task', {
+            'tag' : 'tree', // can be null
+            'first' : 0,
+            'last' : 50,
+            'username' : userManager.getUser().username,
+            'token' : userManager.getUser().token,
+            'action' : 'list'
+          })
+          .success(function(data, status, headers,
+                            config) { $scope.tasks = data['tasks']; })
+          .error(function(data, status, headers,
+                          config) { notificationHub.serverError(status); });
     });
-  });
