de11f0c805f66241dde498d99a32e055bbb2713b
Gemma Martini <martini.gemma3@gmail.com>
2019-05-19 19:42:33 +0200

    minor changes

--- cmsocial-web/scripts/task_tree.js
+++ cmsocial-web/scripts/task_tree.js
@@ -2,21 +2,21 @@
 
 /* Tasks page */
 
-angular.module('cmsocial')
-    .controller('TaskTree', function($scope, $stateParams, $state, $http,
-                                     notificationHub, userManager, l10n,
-                                     API_PREFIX) {
-      $http
-          .post(API_PREFIX + 'task', {
+angular.module('cmsocial').controller('TaskTree', function(
+    $scope, $stateParams, $state, $http, notificationHub,
+    userManager, l10n, API_PREFIX) {
+        $http.post(API_PREFIX + 'task', {
             'tag' : 'tree', // can be null
             'first' : 0,
             'last' : 50,
             'username' : userManager.getUser().username,
             'token' : userManager.getUser().token,
             'action' : 'list'
-          })
-          .success(function(data, status, headers,
-                            config) { $scope.tasks = data['tasks']; })
-          .error(function(data, status, headers,
-                          config) { notificationHub.serverError(status); });
-    });
+        })
+        .success(function(data, status, headers,
+            config) { $scope.tasks = data['tasks']; })
+        .error(function(data, status, headers, config) {
+            notificationHub.serverError(status);
+        });
+    }
+);
