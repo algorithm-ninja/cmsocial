93e2ffb0729ad1a0443fd542f8985ebdf2e0abda
Gemma Martini <martini.gemma3@gmail.com>
2019-05-20 19:31:05 +0200

    fixing color

--- cmsocial-web/scripts/task_tree.js
+++ cmsocial-web/scripts/task_tree.js
@@ -169,20 +169,14 @@ angular.module('cmsocial').controller('TaskTree', function(
                 .attr("transform", function(d) { 
                     return "translate(" + d.x + "," + d.y + ")"; });
 
-                    
-            var singleChart = nodeEnter.append("single-chart");
-            /*var svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            svg1.setAttribute("viewBox", "0 0 36 36")
-            svg1.setAttribute("class", "circular-chart orange");
-            singleChart.append(svg1);*/
-
-            var path1 = singleChart.append('path')
+            var path1 = svg.append('path')
                 .attr("class", "circle-bg")
                 .attr("d", "M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831");
 
-            var path2 = singleChart.append('path')
+            var path2 = svg.append('path')
                 .attr("class", "circle")
                 .attr("stroke-dasharray", "30, 100")
+                .attr("stroke", "#ff9f00")
                 .attr("d", "M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831");
 
             /*<div class="single-chart">
