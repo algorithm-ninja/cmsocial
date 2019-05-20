4b6399b24bb531f0bd1a35d364ed5dd2650b318a
Gemma Martini <martini.gemma3@gmail.com>
2019-05-20 11:59:29 +0200

    tentativo

--- cmsocial-web/scripts/task_tree.js
+++ cmsocial-web/scripts/task_tree.js
@@ -139,24 +139,24 @@ angular.module('cmsocial').controller('TaskTree', function(
                 "name": "Top Level",
                 "parent": "null",
                 "children": [
-                {
-                    "name": "Level 2: A",
-                    "parent": "Top Level",
-                    "children": [
                     {
-                        "name": "Son of A",
-                        "parent": "Level 2: A"
+                        "name": "Level 2: A",
+                        "parent": "Top Level",
+                        "children": [
+                            {
+                                "name": "Son of A",
+                                "parent": "Level 2: A"
+                            },
+                            {
+                                "name": "Daughter of A",
+                                "parent": "Level 2: A"
+                            }
+                        ]
                     },
                     {
-                        "name": "Daughter of A",
-                        "parent": "Level 2: A"
+                        "name": "Level 2: B",
+                        "parent": "Top Level"
                     }
-                    ]
-                },
-                {
-                    "name": "Level 2: B",
-                    "parent": "Top Level"
-                }
                 ]
             };
 
