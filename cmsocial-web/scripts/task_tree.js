5e073f557bb919b8af1eb2c88971de4cf77f97ea
Gemma Martini <martini.gemma3@gmail.com>
2019-05-21 14:18:41 +0200

    log username

--- cmsocial-web/scripts/task_tree.js
+++ cmsocial-web/scripts/task_tree.js
@@ -16,7 +16,8 @@ angular.module('cmsocial').controller('TaskTree', function(
         })
         .success(function(data, status, headers, config) {
             $scope.tasks = data['tasks'];
-            console.log(data.tasks);
+            console.log(username);
+            //console.log(data.tasks);
 
             //Static declaration of needed fields
             var extra_fields = {
