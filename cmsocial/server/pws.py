c8ea7e32ef2944ced4eb67a5bb454bd4420c18c2
William Di Luigi <williamdiluigi@gmail.com>
2021-12-02 21:21:13 +0100

    Allow help for access_level <= 1

--- cmsocial/server/pws.py
+++ cmsocial/server/pws.py
@@ -1185,7 +1185,10 @@ Recovery code: %s""" % (user.username, user.social_user.recover_code)):
             local.resp['id'] = t.id
             local.resp['name'] = t.name
             local.resp['title'] = t.title
-            local.resp['help_available'] = t.social_task.help_available
+            local.resp['help_available'] = (
+                t.social_task.help_available
+                or local.access_level <= 2
+            )
             local.resp['statements'] =\
                 dict([(l, s.digest) for l, s in t.statements.iteritems()])
             local.resp['submission_format'] =\
@@ -1358,13 +1361,13 @@ Recovery code: %s""" % (user.username, user.social_user.recover_code)):
 
         elif local.data['action'] == 'get':
             # Make sure that this task allows requests
-            if not task.social_task.help_available:
+            if local.access_level >= 3 and not task.social_task.help_available:
                 return 'Questo task non accetta richieste di testcase.'
 
             socpart = local.participation.social_participation
             # Make sure that the user is allowed to request
             # TODO: de-hardcode this.
-            if datetime.utcnow() - socpart.last_help_time < timedelta(hours=1):
+            if local.access_level >= 3 and datetime.utcnow() - socpart.last_help_time < timedelta(hours=1):
                 return "Hai già fatto una richiesta nell'ultima ora."
 
             testcase = local.session.query(Testcase)\
