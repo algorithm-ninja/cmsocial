01a2b47a7af0ad546c7b1b45083f83e31f139734
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-10-11 17:42:46 +0200

    Missing updates in the database

--- sql_updaters/from_cms/fix_score_details.py
+++ sql_updaters/from_cms/fix_score_details.py
@@ -16,15 +16,18 @@ con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
 
 with con.cursor() as cur:
     cur.execute("SET session_replication_role = replica")
-    cur.execute("CREATE TEMPORARY TABLE update_submission_results ("
-                "   id integer, "
-                "   dataset_id integer, "
-                "   score_details jsonb, "
-                "   PRIMARY KEY (id, dataset_id)"
-                ")")
+    cur.execute(
+        "CREATE TEMPORARY TABLE update_submission_results ("
+        "   id integer, "
+        "   dataset_id integer, "
+        "   score_details jsonb, "
+        "   PRIMARY KEY (id, dataset_id)"
+        ")"
+    )
     cur.execute("SELECT MAX(submission_id) FROM submission_results")
     max_id = cur.fetchone()[0]
     print("Max id is", max_id)
+    num_updates = 0
     for batch in range(100):
         lb = max_id * batch // 100
         ub = max_id * (batch + 1) // 100
@@ -52,16 +55,47 @@ with con.cursor() as cur:
                             testcase["text"] = json.loads(testcase["text"])
                 else:
                     print("Unknown format!!", sub_id, dataset_id, score_details)
+                if "score_fraction" not in item:
+                    if "score" in item and "max_score" in item:
+                        score = item["score"]
+                        max_score = item["max_score"]
+                        if max_score != 0:
+                            score_fraction = score / max_score
+                        else:
+                            if "testcases" in item:
+                                if all(
+                                    tc.get("outcome", "") == "Correct"
+                                    for tc in item["testcases"]
+                                ):
+                                    score_fraction = 1.0
+                                else:
+                                    score_fraction = 0.0
+                            else:
+                                score_fraction = 1.0
+                    else:
+                        # Subtask score missing, assume not a subtask
+                        continue
+                    item["score_fraction"] = score_fraction
+                    num_updates += 1
             updated += [(sub_id, dataset_id, json.dumps(score_details))]
         print("Inserting %d rows in temp table" % (len(updated)))
-        psycopg2.extras.execute_values(cur, "INSERT INTO update_submission_results (id, dataset_id, score_details) VALUES %s", updated)
+        psycopg2.extras.execute_values(
+            cur,
+            "INSERT INTO update_submission_results (id, dataset_id, score_details) VALUES %s",
+            updated,
+        )
         duration = monotonic() - start
-        print("Took %.3fs, ETA: %.3f" % (duration, duration * (100 - batch - 1)))
+        print(
+            "Took %.3fs, ETA: %.3f (%d updates)"
+            % (duration, duration * (100 - batch - 1), num_updates)
+        )
 
     print("Updating actual data")
-    cur.execute("UPDATE submission_results S "
-                "SET score_details = (SELECT score_details FROM update_submission_results U WHERE U.id = S.submission_id AND U.dataset_id = S.dataset_id) "
-                "WHERE EXISTS (SELECT score_details FROM update_submission_results U WHERE U.id = S.submission_id AND U.dataset_id = S.dataset_id)")
+    cur.execute(
+        "UPDATE submission_results S "
+        "SET score_details = (SELECT score_details FROM update_submission_results U WHERE U.id = S.submission_id AND U.dataset_id = S.dataset_id) "
+        "WHERE EXISTS (SELECT score_details FROM update_submission_results U WHERE U.id = S.submission_id AND U.dataset_id = S.dataset_id)"
+    )
     cur.execute("SET session_replication_role = DEFAULT")
     cur.execute("COMMIT")
 con.close()
