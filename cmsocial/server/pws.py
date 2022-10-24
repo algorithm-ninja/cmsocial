b54e4bb003163a7e1101f3d056c22427684e839f
Dario Petrillo <dario.pk1@gmail.com>
2022-10-24 16:14:48 +0200

    Restore lost changes from c8ea7e32ef2944ced4eb67a5bb454bd4420c18c2

--- cmsocial/server/pws.py
+++ cmsocial/server/pws.py
@@ -1450,13 +1450,13 @@ Recovery code: %s""" % (user.username, user.social_user.recover_code)):
 
         elif local.data['action'] == 'get':
             # Make sure that this task allows requests
-            if not task.social_task.help_available:
+            if local.access_level >= 3 and not task.social_task.help_available:
                 return 'Questo task non accetta richieste di testcase.'
 
             socpart = local.participation.social_participation
             # Make sure that the user is allowed to request
             # TODO: de-hardcode this.
-            if datetime.utcnow() - socpart.last_help_time < timedelta(hours=1):
+            if local.access_level >= 3 and datetime.utcnow() - socpart.last_help_time < timedelta(hours=1):
                 return "Hai già fatto una richiesta nell'ultima ora."
 
             testcase = local.session.query(Testcase)\
